8cb15083fa44584865d54e0193ce6a82a5b155e3
SyureNyanko <dr.myself+github@gmail.com>
2019-06-12 00:57:01 +0900

    parrot

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<span id="app">
+  <parrot></parrot>
+  </span>
+</template>
+<script>
+import Parrot from './components/Parrot.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Parrot
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
